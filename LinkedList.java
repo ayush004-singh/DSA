e18f13f5732107f17d2b40a7cca01ab8edaa5dac
Ayush Singh <aayushkashyapa415@gmail.com>
2025-08-07 23:28:47 +0530

    LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -6,7 +6,28 @@ class Node{
         this.data=data;
         this.next=null;
     }
+// Print List Items
+    public static void printList(Node head){
+        Node curr = head;
+        while(curr!=null){
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+
+    }
+
+    
 }
 public class LinkedList {
+    public static void main(String[] args) {
+        
+
+    Node N = new Node(50);
+    N.next = new Node(60);
+        N.next.next= new Node(70);
+
+        Node.printList(N);
+    
+    }
     
 }
